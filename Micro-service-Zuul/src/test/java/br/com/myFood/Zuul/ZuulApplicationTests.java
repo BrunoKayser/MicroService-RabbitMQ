8b314dc8db450c34c1fe02d936ebc0d3d8692d2b
cwi.bschwertner <cwi.bschwertner@bv.com.br>
2020-10-04 21:31:53 -0300

    zuul - Adicionando properties

---
+++ Micro-service-Zuul/src/test/java/br/com/myFood/Zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.myFood.Zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
